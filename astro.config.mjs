2f224fcea5adda935c7f8f70e6021ff0a1d0524f
JanKuchnia <jaxergames.bussines@gmail.com>
2026-07-06 23:28:38 +0200

    feat: add GitHub deployment workflow and update project build configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://jankuchnia.github.io',
+  base: 'mateo_trans-cars',
+});
